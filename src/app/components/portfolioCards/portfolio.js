ffe5d928f7f090777a530de0604ff72a38fb340d
WebDevilishSmile <webdevilishsmile@gmail.com>
2024-01-09 20:44:39 -0500

    1/9/2024 20:44

--- src/app/components/portfolioCards/portfolio.js
+++ src/app/components/portfolioCards/portfolio.js
@@ -1,5 +1,23 @@
 export const portfolio = [
   {
+    title: 'UPS SLICs',
+    img: '/portfolio/ups.png',
+    details: 'UPS SLICs website',
+    website: 'https://slics-git-master-webdevilishsmile.vercel.app/',
+  },
+  {
+    title: 'Omnifood',
+    img: '/portfolio/omnifood.png',
+    details: 'Omnifood website',
+    website: 'https://omnifood-davila.netlify.app/',
+  },
+  {
+    title: 'Sherry May Cosmetics',
+    img: '/portfolio/smc.png',
+    details: 'Sherry May Cosmetics website',
+    website: 'https://sherry-may.vercel.app/',
+  },
+  {
     title: 'Bankist',
     img: '/portfolio/bankist.png',
     details: 'Bankist website',
@@ -16,29 +34,5 @@ export const portfolio = [
     img: '/portfolio/natours.png',
     details: 'Natours website',
     website: 'https://natours-davila.netlify.app/',
-  },
-  {
-    title: 'Omnifood',
-    img: '/portfolio/omnifood.png',
-    details: 'Omnifood website',
-    website: 'https://omnifood-davila.netlify.app/',
-  },
-  {
-    title: 'Modern React',
-    img: '/portfolio/react.png',
-    details: 'Modern React website',
-    website: 'https://modernreact-davila.netlify.app/',
-  },
-  {
-    title: 'Sherry May Cosmetics',
-    img: '/portfolio/smc.png',
-    details: 'Sherry May Cosmetics website',
-    website: 'https://sherry-may.vercel.app/',
-  },
-  {
-    title: 'UPS SLICs',
-    img: '/portfolio/ups.png',
-    details: 'UPS SLICs website',
-    website: 'https://slics-git-master-webdevilishsmile.vercel.app/',
   },
 ];
